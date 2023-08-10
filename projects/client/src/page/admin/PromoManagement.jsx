0476416c766f0a024b8201005728a5ed2ad489a0
hafizhbimo <hfzhbimo@gmail.com>
2023-08-10 14:44:06 +0700

    add promo management page

---
+++ projects/client/src/page/admin/PromoManagement.jsx
@@ -0,0 +1,18 @@
+
+
+
+const PromoManagement = () => {
+  const tabList = [
+    { name: "My Discount", icon: "", isActive: true },
+    { name: "Create Discount", icon: "", isActive: false },
+    { name: "My Voucher", icon: "", isActive: false },
+    { name: "Create Voucher", icon:"" , isActive: false },
+  ];
+  return (
+    <div>
+      halo
+    </div>
+  );
+};
+
+export default PromoManagement;
