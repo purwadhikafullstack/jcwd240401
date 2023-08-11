c6311efd9bd8cd00fd1de79400d915a31b7a1b37
vanessangelo <vanessagreenangelo@gmail.com>
2023-08-11 20:16:04 +0700

    revised reusable component

--- projects/client/src/component/Footer.jsx
+++ projects/client/src/component/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { HiOutlinePlusSm } from 'react-icons/hi'
 
 export default function Footer() {
     return (
@@ -8,7 +7,7 @@ export default function Footer() {
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <a href="3" className="flex items-center mb-4 sm:mb-0">
-                            <img src="/Logo Groceer-e (White).svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                            <img src="/Logo Groceer-e (White).svg" className="h-8 mr-3" alt="Flowbite Logo" />
                         </a>
                         <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 dark:text-gray-400">
                             <li>
