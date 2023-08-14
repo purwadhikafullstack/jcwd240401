28800cd239539c9e878187f4fdefabb165a0f718
tristanashari <tris.ashari@gmail.com>
2023-08-14 11:57:58 +0700

    adding redux state, upadates on FE and BE auth

--- projects/client/src/page/admin/AdminHome.jsx
+++ projects/client/src/page/admin/AdminHome.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Sidebar from '../../component/Sidebar'
 import AdminHomeContent from '../../component/admin/AdminHomeContent'
 import Footer from '../../component/Footer'
+import { useSelector } from 'react-redux'
 
 export default function AdminHome() {
+    const token = localStorage.getItem("token")
+    const profile = useSelector((state) => state.auth.profile)
+    const role = Number(profile.role)
     return (
         <div className=" grid grid-rows-7 min-h-screen">
-            <div className=" row-span-2 col-span-1 min-h-screen"><Sidebar roleId={1}/></div>
+            <div className=" row-span-2 col-span-1 min-h-screen"><Sidebar roleId={role}/></div>
             <div className=" row-span-2 col-span-2 flex justify-center w-full  min-h-screen">
                 <AdminHomeContent />
             </div>
--- projects/client/src/page/user/Home.jsx
+++ projects/client/src/page/user/Home.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 export default function Home() {
+    const [latitude, setLatitude] = useState("")
+    const [longitude, setLongitude] = useState("")
+    const [error, setError] = useState("")
+    const [nearestBranch, setNearestBranch] = useState([])
+
+    useEffect(() => {
+        const askForLocationPermission = async () => {
+        const permissionGranted = await new Promise((resolve) => {
+        const consent = window.confirm(
+            "Do yo allow this app to access your location?"
+        );
+        resolve(consent);
+        });
+
+        if (permissionGranted) {
+            navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+                setError(null);
+            },
+            (error) => {
+                console.error("Error getting geolocation:", error.message);
+                setError("Error getting geolocation. Please allow location access.");
+            }
+        );
+        } else {
+            setError("Location access denied.");
+        }
+    };
+
+        if ("geolocation" in navigator) {
+            askForLocationPermission();
+        } else {
+            setError("Geolocation is not supported by your browser.");
+        }
+    }, []);
+
+    console.log(latitude, longitude)
+
+    useEffect(() => {
+        try{
+            axios.get(`http://localhost:8000/api/auth/nearest-branch?latitude=${latitude}&longitude=${longitude}`).then((response) => setNearestBranch(response.data))
+        }catch(error){
+            console.log(error)
+        }
+    }, [latitude, longitude])
+
+    console.log(nearestBranch)
+
     return (
+        <>
         <div>Home</div>
+        </>
     )
 }
