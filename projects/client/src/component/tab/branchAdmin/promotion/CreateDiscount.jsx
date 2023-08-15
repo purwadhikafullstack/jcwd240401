660e520838a2f85b855cfe5c17061bd5a2660c4a
hafizhbimo <hfzhbimo@gmail.com>
2023-08-15 18:37:16 +0700

    refactor handlechange, add formik & yup

--- projects/client/src/component/tab/branchAdmin/promotion/CreateDiscount.jsx
+++ projects/client/src/component/tab/branchAdmin/promotion/CreateDiscount.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 import getAllDiscountType from "../../../../api/promotion";
 import InputField from "../../../InputField";
@@ -35,14 +37,8 @@ export default function CreateDiscount() {
     setCreateDiscount({ ...createDiscount, discount_type_id: obj.value });
   };
 
-  const handleChangeAmount = (event) => {
-    console.log(event.target.value);
-    setCreateDiscount({ ...createDiscount, amount: event.target.value });
-  };
-
-  const handleChangeExpiredDate = (event) => {
-    console.log(event.target.value);
-    setCreateDiscount({ ...createDiscount, expiredDate: event.target.value });
+  const handleChange = (event, inputField) => {
+    setCreateDiscount({ ...createDiscount, [inputField]: event.target.value });
   };
 
   const handleSubmit = () => {
@@ -51,49 +47,78 @@ export default function CreateDiscount() {
       createDiscount
     );
   };
+
+  const createDiscountSchema = yup
+    .object()
+    .shape({
+      amount: yup
+        .number()
+        .typeError("age must be a number")
+        .positive("age must be greater than zero")
+        .required("age is required"),
+    });
   return (
     <div className="flex flex-col">
       <div>
-        <form onSubmit={handleSubmit}>
-          <div className="w-full">
-            <label htmlFor="email" className="font-inter">
-              Discount type:
-            </label>
-            <CustomDropdown
-              options={dataAllDiscountType}
-              onChange={handleChangeDropdown}
-              placeholder={"--choose discount type--"}
-            />
-          </div>
-          {createDiscount.discount_type_id == 2 || createDiscount.discount_type_id == 3 ? (
-            <div>
-              <label htmlFor="email" className="font-inter">
-                Discount amount:
-              </label>
-              <InputField onChange={handleChangeAmount} />
-            </div>
-          ) : (
-            ""
-          )}
+        <Formik
+          initialValues={{
+            discount_type_id: "",
+            amount: 0,
+            expiredDate: "",
+            branch_id: 1,
+          }}
+          validationSchema={createDiscountSchema}
+          onSubmit={handleSubmit}
+        >
+          {(props) => (
+            <form onSubmit={handleSubmit}>
+              <div className="w-full">
+                <label htmlFor="type" className="font-inter">
+                  Discount type:
+                </label>
+                <CustomDropdown
+                  options={dataAllDiscountType}
+                  onChange={handleChangeDropdown}
+                  placeholder={"--choose discount type--"}
+                />
+              </div>
+              {createDiscount.discount_type_id == 2 ||
+              createDiscount.discount_type_id == 3 ? (
+                <div>
+                  <label htmlFor="amount" className="font-inter">
+                    Discount amount:
+                  </label>
+                  <InputField onChange={(e) => handleChange(e, "amount")} />
+                  {props.errors.amount && props.touched.amount && (
+                    <div className="text-reddanger absolute top-12">
+                      {props.errors.amount}
+                    </div>
+                  )}
+                </div>
+              ) : (
+                ""
+              )}
 
-          <div>
-            <label htmlFor="email" className="font-inter">
-              Expired date
-            </label>
-            <InputField onChange={handleChangeExpiredDate} />
-          </div>
-          <Modal
-            modalTitle={"Create Discount"}
-            toggleName={"Create Discount"}
-            content={`Are you sure to create this category?`}
-            buttonCondition={"positive"}
-            buttonLabelOne={"Cancel"}
-            buttonLabelTwo={"Yes"}
-            buttonTypeOne={"button"}
-            buttonTypeTwo={"submit"}
-            onClickButton={handleSubmit}
-          />
-        </form>
+              <div>
+                <label htmlFor="date" className="font-inter">
+                  Expired date
+                </label>
+                <InputField onChange={(e) => handleChange(e, "expiredDate")} />
+              </div>
+              <Modal
+                modalTitle={"Create Discount"}
+                toggleName={"Create Discount"}
+                content={`Are you sure to create this category?`}
+                buttonCondition={"positive"}
+                buttonLabelOne={"Cancel"}
+                buttonLabelTwo={"Yes"}
+                buttonTypeOne={"button"}
+                buttonTypeTwo={"submit"}
+                onClickButton={handleSubmit}
+              />
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
