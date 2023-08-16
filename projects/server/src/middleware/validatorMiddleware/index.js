efe457b8aa085b8e1e5c55036464c164bc9bd9e0
hafizhbimo <hfzhbimo@gmail.com>
2023-08-16 20:31:53 +0700

    add validator to creat discount&voucher

--- projects/server/src/middleware/validatorMiddleware/index.js
+++ projects/server/src/middleware/validatorMiddleware/index.js
@@ -21,12 +21,8 @@ const validate = (validations) => {
 
 module.exports = {
   validateLogin: validate([
-    body("email")
-    .notEmpty()
-    .withMessage("Email is required"),
-    body("password")
-    .notEmpty()
-    .withMessage("Password is required")
+    body("email").notEmpty().withMessage("Email is required"),
+    body("password").notEmpty().withMessage("Password is required"),
   ]),
   validateRegisterAdmin: validate([
     body("name")
@@ -39,25 +35,19 @@ module.exports = {
       .withMessage("incorrect email format")
       .notEmpty()
       .withMessage("email is required"),
-    body("phone")
-      .notEmpty()
-      .withMessage("phone number is required"),
-    body("province")
-      .notEmpty()
-      .withMessage("Branch province is required"),
-    body("city")
-      .notEmpty()
-      .withMessage("Branch city is required")
+    body("phone").notEmpty().withMessage("phone number is required"),
+    body("province").notEmpty().withMessage("Branch province is required"),
+    body("city").notEmpty().withMessage("Branch city is required"),
   ]),
   validateSetPasswordAdmin: validate([
     body("password")
       .isLength({ min: 8 })
       .withMessage("minimum password length is 8 characters")
       .isStrongPassword({
-          minSymbols: 0,
+        minSymbols: 0,
       })
       .withMessage(
-          "password must contain 1 uppercase, 1 lowercase and 1 number"
+        "password must contain 1 uppercase, 1 lowercase and 1 number"
       ),
     body("confirmPassword")
       .notEmpty()
@@ -74,5 +64,64 @@ module.exports = {
     body("discount_type_id")
       .notEmpty()
       .withMessage("discount type is required"),
+    body("amount").isNumeric().withMessage("amount has to be numeric"),
+    body("expiredDate")
+      .notEmpty()
+      .withMessage("expired date is required")
+      .isDate()
+      .withMessage("Invalid date format")
+      .custom((value) => {
+        const currentDate = new Date();
+        const selectedDate = new Date(value);
+        if (selectedDate < currentDate) {
+          throw new Error("expired date cannot be in the past");
+        }
+        return true;
+      }),
+
+    body("amount")
+      .if(body("discount_type_id").not().equals(1))
+      .notEmpty()
+      .withMessage("amount cannot be empty")
+      .isNumeric()
+      .withMessage("amount has to be numeric"),
+  ]),
+  validateCreateVoucher: validate([
+    body("voucher_type_id")
+      .isNumeric()
+      .withMessage("voucher type has to be numeric")
+      .notEmpty()
+      .withMessage("voucher type is required"),
+    body("usedLimit")
+      .isNumeric()
+      .withMessage("usedLimit has to be numeric")
+      .custom((value) => {
+        if (value <= 0) {
+          throw new Error("usedLimit must be grater than zero");
+        }
+        return true;
+      }),
+    body("amount")
+      .if(body("voucher_type_id").not().equals(1))
+      .notEmpty()
+      .withMessage("amount cannot be empty")
+      .isNumeric()
+      .withMessage("amount has to be numeric"),
+    body("expiredDate")
+      .notEmpty()
+      .withMessage("expired date is required")
+      .isDate()
+      .withMessage("Invalid date format")
+      .custom((value) => {
+        const currentDate = new Date();
+        const selectedDate = new Date(value);
+        if (selectedDate < currentDate) {
+          throw new Error("expired date cannot be in the past");
+        }
+        return true;
+      }),
+    body("minTransaction").isNumeric().withMessage("amount has to be numeric"),
+    body("maxDiscount").isNumeric().withMessage("amount has to be numeric"),
+    body("isReferral").isBoolean().withMessage("isReferral has to be boolean"),
   ]),
 };
--- projects/server/src/routes/adminRouter.js
+++ projects/server/src/routes/adminRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const productController = require("../controllers/productController");
 const { admin: adminController } = require("../controllers");
 const multerMiddleware = require("../middleware/multerMiddleware/category");
-const promoValidator = require("../middleware/validatorMiddleware")
+const promoValidator = require("../middleware/validatorMiddleware");
 
 // create category
 router.post(
@@ -10,10 +10,18 @@ router.post(
   multerMiddleware.single("file"),
   productController.createCategory
 );
-router.post("/discounts", promoValidator.validateCreateDiscount, adminController.createDiscount);
+router.post(
+  "/discounts",
+  promoValidator.validateCreateDiscount,
+  adminController.createDiscount
+);
 router.get("/discounts", adminController.getAllDiscount);
 router.get("/discount-types", adminController.getAllDiscountType);
-router.post("/vouchers", adminController.createVoucher);
+router.post(
+  "/vouchers",
+  promoValidator.validateCreateVoucher,
+  adminController.createVoucher
+);
 router.get("/vouchers", adminController.getAllVoucher);
 router.get("/voucher-types", adminController.getAllVoucherType);
 
