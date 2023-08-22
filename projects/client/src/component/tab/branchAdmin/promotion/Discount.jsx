9b47cab44cae046b0d6e95ccf95be0801e582731
hafizhbimo <hfzhbimo@gmail.com>
2023-08-22 16:12:46 +0700

    switching position for create and list disc&vouch

--- projects/client/src/component/tab/branchAdmin/promotion/Discount.jsx
+++ projects/client/src/component/tab/branchAdmin/promotion/Discount.jsx
@@ -4,7 +4,7 @@ import AllDiscount from "../../../accordion/promotion/AllDiscount";
 import CreateDiscount from "../../../accordion/promotion/CreateDiscount";
 
 export default function Discount() {
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState("all");
 
   const handleTabClick = (name) => {
     setActiveTab((prevTab) => (prevTab === name ? '' : name));
@@ -14,19 +14,19 @@ export default function Discount() {
     <>
       <CustomAccordion
         activeTab={activeTab}
-        tabId="all"
-        title="All Discount"
-        onClick={handleTabClick}
-      >
-        <AllDiscount />
-      </CustomAccordion>
-      <CustomAccordion
-        activeTab={activeTab}
         tabId="create"
         title="Create New Discount"
         onClick={handleTabClick}
       >
         <CreateDiscount />
+      </CustomAccordion>
+      <CustomAccordion
+        activeTab={activeTab}
+        tabId="all"
+        title="All Discount"
+        onClick={handleTabClick}
+      >
+        <AllDiscount />
       </CustomAccordion>
     </>
   );
--- projects/client/src/component/tab/branchAdmin/promotion/Voucher.jsx
+++ projects/client/src/component/tab/branchAdmin/promotion/Voucher.jsx
@@ -4,7 +4,7 @@ import AllVoucher from "../../../accordion/promotion/AllVoucher";
 import CreateVoucher from "../../../accordion/promotion/CreateVoucher";
 
 export default function Voucher() {
-  const [activeTab, setActiveTab] = useState("");
+  const [activeTab, setActiveTab] = useState("all");
 
   const handleTabClick = (name) => {
     setActiveTab((prevTab) => (prevTab === name ? "" : name));
@@ -14,19 +14,19 @@ export default function Voucher() {
     <>
       <CustomAccordion
         activeTab={activeTab}
-        tabId="all"
-        title="All Voucher"
-        onClick={handleTabClick}
-      >
-        <AllVoucher />
-      </CustomAccordion>
-      <CustomAccordion
-        activeTab={activeTab}
         tabId="create"
         title="Create New Voucher"
         onClick={handleTabClick}
       >
         <CreateVoucher />
+      </CustomAccordion>
+      <CustomAccordion
+        activeTab={activeTab}
+        tabId="all"
+        title="All Voucher"
+        onClick={handleTabClick}
+      >
+        <AllVoucher />
       </CustomAccordion>
     </>
   );
