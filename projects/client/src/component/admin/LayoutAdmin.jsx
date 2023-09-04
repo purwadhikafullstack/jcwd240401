21331ddcaddc948d7f04e7f5768aeff27beb7e5f
vanessangelo <vanessagreenangelo@gmail.com>
2023-09-04 14:01:14 +0700

    revised layoutadmin

--- projects/client/src/component/admin/LayoutAdmin.jsx
+++ projects/client/src/component/admin/LayoutAdmin.jsx
@@ -3,17 +3,17 @@ import Sidebar from "../Sidebar";
 
 export default function LayoutAdmin(props) {
   return (
-    <div className="grid min-h-screen w-full">
-      <div className="min-h-screen flex gap-2">
-        <div className="lg:basis-1/6 shadow-md grid">
-          <Sidebar />
-        </div>
+    <div className="flex min-h-screen w-full">
+      <div className="shadow-md grid h-screen fixed left-0 z-50">
+        <Sidebar />
+      </div>
+      <div className="min-h-screen flex flex-col gap-2 w-full lg:ml-64">
         <div className="flex-grow flex justify-center">
           {props.children}
         </div>
-      </div>
-      <div className="">
-        <Footer />
+        <div className="">
+          <Footer />
+        </div>
       </div>
     </div>
   );
