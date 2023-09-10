2575dcaa7d4cfcd3d091ab21093b9538f477d6ae
vanessangelo <vanessagreenangelo@gmail.com>
2023-09-11 01:54:17 +0700

    revised url handling tabs and sidebar

--- projects/client/src/component/admin/SuperAdminManageProductContent.jsx
+++ projects/client/src/component/admin/SuperAdminManageProductContent.jsx
@@ -10,8 +10,8 @@ export default function SuperAdminManageProductContent() {
     const [content, setContent] = useState(<AllProduct />);
     const title = "Manage Product";
     const tabList = [
-        { name: "My Product", icon: <LuFolderOpen size={25} />, isActive: false, tab: <AllProduct /> },
-        { name: "Create Product", icon: <LuFolderPlus size={25} />, isActive: false, tab: <CreateProduct /> },
+        { name: "All Product", icon: <LuFolderOpen size={25} />, isActive: false, param: "all-product", tab: <AllProduct /> },
+        { name: "Create Product", icon: <LuFolderPlus size={25} />, isActive: false, param: "create-product", tab: <CreateProduct /> },
     ];
     return (
         <div className="flex flex-col w-9/12 py-4">
