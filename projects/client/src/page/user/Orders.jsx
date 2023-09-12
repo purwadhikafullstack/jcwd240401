2aba7bc37aac49d4c60faaece8bce7419fde786e
hafizhbimo <hfzhbimo@gmail.com>
2023-09-12 09:10:14 +0700

    add navbar for orders page

--- projects/client/src/page/user/Orders.jsx
+++ projects/client/src/page/user/Orders.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import UserOrderContent from "../../component/user/UserOrderContent";
+import NavbarTop from "../../component/NavbarTop";
+import NavbarBottom from "../../component/NavbarBottom";
 
 export default function Orders() {
   return (
     <div>
-      <UserOrderContent />
+      <NavbarTop />
+      <div>
+        <UserOrderContent />
+      </div>
+      <NavbarBottom />
     </div>
   );
 }
