39cb9ff99e3101d3b38cb59d500ad81aaf7da3c6
hafizhbimo <hfzhbimo@gmail.com>
2023-09-19 04:57:44 +0700

    add cart counter at navbar

--- projects/client/src/component/NavbarTop.jsx
+++ projects/client/src/component/NavbarTop.jsx
@@ -1,57 +1,91 @@
-import React, {useState, useEffect} from 'react'
-import { useSelector } from 'react-redux'
-import logo from '../assets/logo_Groceer-e.svg'
-import { HiOutlineLocationMarker, HiOutlineShoppingCart } from 'react-icons/hi'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import Button from './Button'
-import background from '../assets/BackgroundLeaves.jpg'
-import DropdownForNavbar from './user/DropdownForNavbar'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import logo from "../assets/logo_Groceer-e.svg";
+import { HiOutlineLocationMarker, HiOutlineShoppingCart } from "react-icons/hi";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import Button from "./Button";
+import background from "../assets/BackgroundLeaves.jpg";
+import DropdownForNavbar from "./user/DropdownForNavbar";
 
-export default function NavbarTop({city, province}) {
-    const token = localStorage.getItem("token")
-    const profile = useSelector((state) => state.auth.profile)
-    const navigate = useNavigate()
-    const location = useLocation()
+export default function NavbarTop({ city, province }) {
+  const cartItems = useSelector((state) => state.cart.cart);
+  const token = localStorage.getItem("token");
+  const profile = useSelector((state) => state.auth.profile);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const defaultRoutes = [
-        {menu: "Home", to: "/"},
-        {menu: "Orders", to: "/user/orders"},
-        {menu: "Cart", to: "/user/cart"},
-        {menu: "Account", to: "/user/account"}
-    ]
-    const loggedInRoutes = [
-        {menu: "Home", to: "/"},
-        {menu: "Orders", to: "/user/orders"},
-        {menu: <HiOutlineShoppingCart size={25}/>, to: "/user/cart"},
-        {menu: <DropdownForNavbar/>}
-    ]
+  const defaultRoutes = [
+    { menu: "Home", to: "/" },
+    { menu: "Orders", to: "/user/orders" },
+    { menu: "Cart", to: "/user/cart" },
+    { menu: "Account", to: "/user/account" },
+  ];
+  const loggedInRoutes = [
+    { menu: "Home", to: "/" },
+    { menu: "Orders", to: "/user/orders" },
+    { menu: <HiOutlineShoppingCart size={25} />, to: "/user/cart" },
+    { menu: <DropdownForNavbar /> },
+  ];
 
-    const routes = token ? loggedInRoutes : defaultRoutes
+  const routes = token ? loggedInRoutes : defaultRoutes;
 
-    const onClickLogIn = () => {
-        navigate("/login")
-    }
+  const onClickLogIn = () => {
+    navigate("/login");
+  };
   return (
     <>
-    <div className='hidden lg:w-full lg:h-24 lg:shadow-md lg:bg-cover lg:bg-center lg:grid lg:grid-cols-2 lg:px-10' style={{backgroundImage: `url(${background})`, backgroundSize: 'cover'}}>
+      <div
+        className="hidden lg:w-full lg:h-24 lg:shadow-md lg:bg-cover lg:bg-center lg:grid lg:grid-cols-2 lg:px-10"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+        }}
+      >
         <div className="w-full h-full col-span-1 grid grid-cols-2 items-center">
-            <div><Link to="/"><img src={logo} alt="logo" /></Link></div>
-            <div className='flex gap-4 items-center'>
-                <HiOutlineLocationMarker className="w-6 h-6" />
-                <div>{ city && province ? `${city}, ${province}` : ""}</div> 
-            </div>
+          <div>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
+          </div>
+          <div className="flex gap-4 items-center">
+            <HiOutlineLocationMarker className="w-6 h-6" />
+            <div>{city && province ? `${city}, ${province}` : ""}</div>
+          </div>
         </div>
         <div className="w-full h-full col-span-1 flex justify-end gap-20 items-center font-inter">
-            <div className="flex items-center justify-between gap-10">
-                {routes.map(({ menu, to }, idx) => (
-                    <Link to={to} className={`h-10 flex items-center justify-center ${location.pathname === to ? `text-maingreen font-bold border-b-2 border-maingreen` : `text-darkgrey`}`}>
-                    <div key={idx}>{menu}</div>
-                    </Link>
-                ))}
+          <div className="flex items-center justify-between gap-10">
+            {routes.map(({ menu, to }, idx) => (
+              <Link
+                to={to}
+                className={`h-10 flex items-center justify-center ${
+                  location.pathname === to
+                    ? `text-maingreen font-bold border-b-2 border-maingreen`
+                    : `text-darkgrey`
+                }`}
+              >
+                <div key={idx}>{menu}</div>
+              </Link>
+            ))}
+            {cartItems.length > 0 && (
+              <span
+                className="absolute top-7 right-28 text-white text-xs px-1 rounded-full bg-reddanger"
+                
+              >
+                {cartItems.length}
+              </span>
+            )}
+          </div>
+          {token && profile.role === "3" ? null : (
+            <div className="w-24">
+              <Button
+                condition={"positive"}
+                label={"Log In"}
+                onClick={onClickLogIn}
+              />
             </div>
-            {(token && profile.role === "3") ? null : <div className="w-24"><Button condition={"positive"} label={"Log In"} onClick={onClickLogIn}/></div>}
+          )}
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
