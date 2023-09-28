6abe68d50a66a87c6b2b5975d664ed3ecd5e646f
vanessangelo <vanessagreenangelo@gmail.com>
2023-09-28 14:20:15 +0700

    revised env BE

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: `${__dirname}../.env` });
+require("dotenv").config({ path: `${__dirname}../../.env` });
 
 module.exports = {
   development: {
@@ -7,20 +7,22 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: "mysql",
+    dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_PORT,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_PORT,
   },
 };
